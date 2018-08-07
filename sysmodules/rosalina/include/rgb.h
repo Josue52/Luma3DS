1eb6e9e5c786f28cc424622c0da40de9805fc5dc
Josue52 <32101715+Josue52@users.noreply.github.com>
2018-08-07 17:04:25 -0400

    Added more colors

---
+++ sysmodules/rosalina/include/rgb.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <3ds/types.h>
+#include <3ds/gfx.h>
+#include "utils.h"
+
+#define COLOR_SEL           RGB565(0xD7, 0x9A, 0x28)
+#define COLOR_GREY          RGB565(0x77, 0x77, 0x77)
+#define COLOR_ORANGE        RGB565(0xFF, 0xA5, 0x00)
+#define COLOR_DARKRED       RGB565(0x80, 0x00, 0x00)
+#define COLOR_BRIGHTBLUE    RGB565(0x30, 0x30, 0xFF)
+#define COLOR_BRIGHTYELLOW  RGB565(0xFF, 0xFF, 0x30)
+#define COLOR_CYAN          RGB565(0x00, 0xFF, 0xFF)
+#define COLOR_VIOLET        RGB565(0xEE, 0x82, 0x44)
+#define COLOR_DARGREEN      RGB565(0x00, 0x64 ,0x00)
+#define COLOR_TOMATO        RGB565(0xFF, 0x63, 0x47)
+#define COLOR_LIME          RGB565(0x00, 0xFF, 0x00)
+#define COLOR_CORAL         RGB565(0xFF, 0x7F, 0x50)
+#define COLOR_AZURE         RGB565(0xF0, 0xFF, 0xFF)
